c45c9552935c75452c7be1d915929297fc838c9f
xzr1995317 <450327147@qq.com>
2020-11-18 08:54:44 +0800

    添加修改文件名

--- src/main/java/com/hwkj/search/controller/FileController.java
+++ src/main/java/com/hwkj/search/controller/FileController.java
@@ -69,8 +69,6 @@ public class FileController {
 
             //获取输入流
             inputStream = fileService.getFileInputStream(path);
-//            response.reset();
-//            response.setContentType("application/x-msdownload");
             response.setHeader("Content-Disposition", "attachment;filename=" + EncodingUtils.convertToFileName(request, fileName));
             // 获取输出流
             outputStream = response.getOutputStream();
@@ -101,33 +99,5 @@ public class FileController {
     @PostMapping(value = "/breakpoint-upload", consumes = "multipart/*", headers = "content-type=multipart/form-data", produces = "application/json;charset=UTF-8")
     public RestResponse<Object> breakpointResumeUpload(UploadFileParam param, HttpServletRequest request) {
         return RestResponses.newResponseFromResult(fileService.breakpointResumeUpload(param, request));
-    }
-
-    /**
-     * 文件的内容类型
-     */
-    private static String getFileContentType(String name){
-        String result = "";
-        String fileType = name.toLowerCase();
-        if (fileType.endsWith(".png")) {
-            result = "image/png";
-        } else if (fileType.endsWith(".gif")) {
-            result = "image/gif";
-        } else if (fileType.endsWith(".jpg") || fileType.endsWith(".jpeg")) {
-            result = "image/jpeg";
-        } else if(fileType.endsWith(".svg")){
-            result = "image/svg+xml";
-        }else if (fileType.endsWith(".doc")) {
-            result = "application/msword";
-        } else if (fileType.endsWith(".xls")) {
-            result = "application/x-excel";
-        } else if (fileType.endsWith(".zip")) {
-            result = "application/zip";
-        } else if (fileType.endsWith(".pdf")) {
-            result = "application/pdf";
-        } else {
-            result = "application/octet-stream";
-        }
-        return result;
     }
 }
